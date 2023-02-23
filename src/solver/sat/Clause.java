58ab640543551b6bbf05eac3182e78178b6c4e0b
chenxin_liu <chenxin_liu@brown.edu>
2023-02-22 19:49:33 -0500

    remove original redundant code

--- src/solver/sat/Clause.java
+++ src/solver/sat/Clause.java
@@ -3,14 +3,8 @@ import java.util.*;
 public class Clause {
 	// all literals in this clause
 	public Set<Integer> literals = new HashSet<>();
-	public boolean clauseSatisfied = false;
 	public Clause(Set<Integer> clause) {
 			this.literals = clause;
-			clauseSatisfied = false;
 
-	}
-
-	boolean isUnitClause() {
-			return !clauseSatisfied && literals.size() == 1;
 	}
 }
